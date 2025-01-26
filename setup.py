f2dd60cb1a9e56e7bdf7bc501ddc2aed7d050e1e
Lakshaygoel4321 <iamlakshaygoel5990@gmail.com>
2025-01-26 09:09:50 +0530

    install requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from  setuptools import setup,find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Lakshay",
+    author_email="iamlakshaygoel5990@gmail.com",
+    packages=find_packages()
+    
+)
